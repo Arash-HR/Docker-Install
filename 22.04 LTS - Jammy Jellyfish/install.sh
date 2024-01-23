4e16ce12d788d0a09cc200da47b14e20880f270a
Arash hosseini Rezaei <arashhosseinirezaei@gmail.com>
2024-01-23 15:20:42 +0330

    initial

---
+++ 22.04 LTS - Jammy Jellyfish/install.sh
@@ -0,0 +1,5 @@
+sudo dpkg -i ./containerd.io_1.6.27-1_amd64.deb \
+  ./docker-ce_25.0.0-1~ubuntu.22.04~jammy_amd64.deb \
+  ./docker-ce-cli_25.0.0-1~ubuntu.22.04~jammy_amd64.deb \
+  ./docker-buildx-plugin_0.12.1-1~ubuntu.22.04~jammy_amd64.deb \
+  ./docker-compose-plugin_2.24.1-1~ubuntu.22.04~jammy_amd64.deb
